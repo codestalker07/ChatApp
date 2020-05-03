70f61ac5dd3e588d606da12bcd3fa515d66f4b75
sam <samignesh1998@gmail.com>
2020-05-04 01:01:03 +0530

    Sending Message and it's Store only in Firebase

--- app/src/main/java/com/example/finalapp/MessageActivity.java
+++ app/src/main/java/com/example/finalapp/MessageActivity.java
@@ -8,7 +8,10 @@ import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 //import android.widget.Toolbar;
 import androidx.appcompat.widget.Toolbar;
 
@@ -23,6 +26,8 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.mikhaellopez.circularimageview.CircularImageView;
 
+import java.util.HashMap;
+
 public class MessageActivity extends AppCompatActivity {
 
     CircularImageView profile_image;
@@ -30,6 +35,9 @@ public class MessageActivity extends AppCompatActivity {
 
     FirebaseUser fuser;
     DatabaseReference reference;
+
+    ImageButton btn_send;
+    EditText text_send;
 
     Intent intent;
     Toolbar toolbar;
@@ -53,11 +61,30 @@ public class MessageActivity extends AppCompatActivity {
         });
         profile_image=findViewById(R.id.profile_image);
         username=findViewById(R.id.username);
+        btn_send=findViewById(R.id.btn_send);
+        text_send=findViewById(R.id.text_send);
 
         intent=getIntent();
-        String userid=intent.getStringExtra("userid");
+        final String userid=intent.getStringExtra("userid");
 
         fuser= FirebaseAuth.getInstance().getCurrentUser();
+
+        btn_send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String msg=text_send.getText().toString();
+                if(!msg.equals(""))
+                {
+                    sendMessage(fuser.getUid(),userid,msg);
+                }
+                else
+                {
+                    Toast.makeText(MessageActivity.this,"You can't send empty message",Toast.LENGTH_SHORT).show();
+                }
+                text_send.setText("");
+            }
+        });
+
         reference= FirebaseDatabase.getInstance().getReference("Users").child(userid);
 
         reference.addValueEventListener(new ValueEventListener() {
@@ -82,6 +109,15 @@ public class MessageActivity extends AppCompatActivity {
         });
     }
 
-    //private void setSupportActionBar(Toolbar toolbar) {
-    //}
+    private void sendMessage(String sender,String receiver,String message)
+    {
+        DatabaseReference reference=FirebaseDatabase.getInstance().getReference();
+        HashMap<String,Object> hashMap=new HashMap<>();
+        hashMap.put("sender",sender);
+        hashMap.put("receiver",receiver);
+        hashMap.put("message",message);
+
+        reference.child("Chats").push().setValue(hashMap);
+    }
+
 }
